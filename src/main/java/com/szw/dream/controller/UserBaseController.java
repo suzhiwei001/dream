2fdcda3f2bb174588c5d1b0e3593f634ebaf4276
15802 <759201272@qq.com>
2021-08-19 08:53:54 +0800

    test

--- src/main/java/com/szw/dream/controller/UserBaseController.java
+++ src/main/java/com/szw/dream/controller/UserBaseController.java
@@ -1,19 +1,22 @@
 package com.szw.dream.controller;
 
-import com.szw.dream.po.UserBase;
-import com.szw.dream.service.UserBaseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserBaseController {
-    @Autowired
-    private UserBaseService userBaseService;
-    @RequestMapping("queryUserBaseUid")
+//    @Autowired
+//    private UserBaseService userBaseService;
+
+/*    @RequestMapping("queryUserBaseUid")
     public UserBase queryUserBaseUid(@RequestBody UserBase userBase){
         System.out.println("--------------------");
         return userBaseService.queryUserBaseByUid(userBase.getUid());
+    }*/
+
+    @RequestMapping("test")
+    public String queryUserBaseUid(){
+        System.out.println("----------测试----------");
+        return "ok";
     }
 }
