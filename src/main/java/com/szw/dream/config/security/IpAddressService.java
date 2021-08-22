76d6a66ce3eb705c9d19424d44a8279797c6c6b4
15802 <759201272@qq.com>
2021-08-22 22:09:34 +0800

    ip处理

--- src/main/java/com/szw/dream/config/security/IpAddressService.java
+++ src/main/java/com/szw/dream/config/security/IpAddressService.java
@@ -14,14 +14,18 @@ public class IpAddressService {
      */
     public static String getIpAddress(HttpServletRequest request){
         String ipAddress = request.getHeader("x-forwarded-for");
+        log.info("x-forwarded-for={}",ipAddress);
         if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getHeader("Proxy-Client-IP");
+            log.info("Proxy-Client-IP={}",ipAddress);
         }
         if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            log.info("WL-Proxy-Client-IP={}",ipAddress);
         }
         if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getRemoteAddr();
+            log.info("RemoteAddr={}",ipAddress);
             if(ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")){
                 //根据网卡取本机配置的IP
                 InetAddress inet=null;
@@ -36,26 +40,11 @@ public class IpAddressService {
         //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
         if(ipAddress!=null && ipAddress.length()>15){ //"***.***.***.***".length() = 15
             if(ipAddress.indexOf(",")>0){
+                log.info("对于通过多个代理的情况入:{}",ipAddress);
                 ipAddress = ipAddress.substring(0,ipAddress.indexOf(","));
+                log.info("对于通过多个代理的情况出:{}",ipAddress);
             }
         }
-        iPAll(request);
         return ipAddress;
-    }
-
-    public static void iPAll(HttpServletRequest request) {
-        log.info("x-forwarded-for={}",request.getHeader("x-forwarded-for"));
-        log.info("Proxy-Client-IP={}",request.getHeader("Proxy-Client-IP"));
-        log.info("WL-Proxy-Client-IP={}",request.getHeader("WL-Proxy-Client-IP"));
-        log.info("WL-Proxy-Client-IP={}",request.getHeader("WL-Proxy-Client-IP"));
-
-        try {
-            byte[] address = InetAddress.getLocalHost().getAddress();
-            log.info("address={}",address);
-            String hostAddress = InetAddress.getLocalHost().getHostAddress();
-            log.info("hostAddress={}",hostAddress);
-        } catch (UnknownHostException e) {
-            log.error("");
-        }
     }
 }
