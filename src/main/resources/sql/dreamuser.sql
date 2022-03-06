fdd8ab383421bcd7be7a10c21f7e5689fbafd38a
su_test <759201272@qq.com>
2022-03-06 23:09:00 +0800

    Changes

--- src/main/resources/sql/dreamuser.sql
+++ src/main/resources/sql/dreamuser.sql
@@ -1,7 +1,20 @@
-CREATE TABLE IF NOT EXISTS `runoob_tbl`(
-                                           `runoob_id` INT UNSIGNED AUTO_INCREMENT,
-                                           `runoob_title` VARCHAR(100) NOT NULL,
-    `runoob_author` VARCHAR(40) NOT NULL,
-    `submission_date` DATE,
-    PRIMARY KEY ( `runoob_id` )
-    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table user_base
+(
+    uid              int auto_increment
+        primary key,
+    user_role        int         null,
+    register_source  int         null,
+    user_name        varchar(10) null,
+    nick_name        varchar(10) null,
+    gender           varchar(10) null,
+    birthday         varchar(10) null,
+    signature        varchar(10) null,
+    mobile           varchar(10) null,
+    mobile_bind_time timestamp   null,
+    email            varchar(10) null,
+    email_bind_time  timestamp   null,
+    face             varchar(10) null,
+    create_time      timestamp   null,
+    update_time      timestamp   null,
+    push_token       varchar(10) null
+);
